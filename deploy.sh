4b3b3e1f48a6d662fbb0ce70d501b0febb9d1fd0
ltzone <ltzhou@sjtu.edu.cn>
2021-01-27 21:25:09 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 git pull origin master
 npm install
 npm run build
+if [ $? -ne 0 ]; then
+    echo "build failed"
+else
+    rm -rf ~/www
+    mkdir ~/www
+    cp -R docs/.vuepress/dist/* ~/www
+fi
